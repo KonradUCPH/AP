9d00fb9cda00cb5ea9fa82c5572cfbff557429c8
SteffenCzolbe <steffen.czolb@gmail.com>
2017-09-19 21:00:12 +0200

    basic code done

--- assignment1/src/SubsInterpreter.hs
+++ assignment1/src/SubsInterpreter.hs
@@ -45,14 +45,15 @@ initialContext = (Map.empty, initialPEnv)
 newtype SubsM a = SubsM {runSubsM :: Context -> Either Error (a, Env)}
 
 instance Functor SubsM where
-  fmap = liftM 
+  fmap = liftM
   -- implementation stolen from the testing lecture
 
 instance Applicative SubsM where
   pure = return
-  af <*> ax = do f <- af
-                 x <- ax
-                 return (f x)
+  af <*> ax = do
+                f <- af
+                x <- ax
+                return (f x)
   -- implementation stolen from the testing lecture
 
 instance Monad SubsM where
@@ -62,7 +63,7 @@ instance Monad SubsM where
   m >>= f = SubsM ( \c -> case runSubsM m c of
                             Left e          -> Left e
                             Right (a, env') -> let (_, penv) = c
-                                                   c' = (env', penv) 
+                                                   c' = (env', penv)
                                                    -- building a new context
                                                in runSubsM (f a) c'
                           )
@@ -77,6 +78,8 @@ pMkArray _ = Left "Array() called with wrong number or type of arguments"
 pAdd :: Primitive
 pAdd [IntVal x, IntVal y] = return $ IntVal $ x + y
 pAdd [StringVal x, StringVal y] = return $ StringVal $ x ++ y
+pAdd [StringVal x, IntVal y] = return $ StringVal $ x ++ show y
+pAdd [IntVal x, StringVal y] = return $ StringVal $ show x ++ y
 pAdd _ = Left "Add() called with wrong number or type of arguments"
 
 pMul :: Primitive
@@ -92,7 +95,7 @@ pLT [IntVal x, IntVal y] | x < y = return TrueVal
 pLT [IntVal _, IntVal _] = return FalseVal
 pLT [StringVal x, StringVal y] | x < y = return TrueVal
 pLT [StringVal _, StringVal _] = return FalseVal
-pLT _ = Left "Less than() called with wrong number or type of arguments"
+pLT _ = Left "LessThan() called with wrong number or type of arguments"
 
 pMod :: Primitive
 pMod [IntVal x, IntVal y] = return $ IntVal $ mod x y
@@ -103,12 +106,14 @@ pCompare [x, y] | x == y = return TrueVal
 pCompare [_, _] = return FalseVal
 pCompare _ = Left "Compare() called with wrong number or type of arguments"
 
-
 modifyEnv :: (Env -> Env) -> SubsM ()
 modifyEnv f = SubsM (\(env, _) -> Right ( (), f env))
 
 putVar :: Ident -> Value -> SubsM ()
 putVar name val = modifyEnv $ Map.insert name val
+
+deleteVar :: Ident -> SubsM ()
+deleteVar name= modifyEnv $ Map.delete name
 
 getVar :: Ident -> SubsM Value
 getVar name = SubsM (\(env, _) ->  -- QUESTION: how to use return and fail here?
@@ -117,6 +122,14 @@ getVar name = SubsM (\(env, _) ->  -- QUESTION: how to use return and fail here?
                   Nothing -> Left ("Variable used but not defined: \""
                                     ++ name ++ "\" ")
                 )
+
+-- gets a Maybe value instead of throwing an error in case of failure
+getMaybeVar :: Ident -> SubsM (Maybe Value)
+getMaybeVar name = SubsM (\(env, _) ->
+                    case Map.lookup name env of
+                      (Just v) -> Right (Just v, env)
+                      Nothing -> Right (Nothing, env)
+                    )
 
 
 getFunction :: FunName -> SubsM Primitive
@@ -130,31 +143,70 @@ getFunction name = SubsM (\(env, penv) ->
 evalExpr :: Expr -> SubsM Value
 evalExpr (Number n) = return (IntVal n)
 evalExpr (String s) = return (StringVal s)
-evalExpr (Array []) = return (ArrayVal []) --TODO: do better with mapM
-evalExpr (Array (x:xs)) = do v <- evalExpr x
-                             (ArrayVal vs) <- evalExpr (Array xs)
-                             return (ArrayVal (v:vs))
-evalExpr Undefined = fail "expression undefined."
+evalExpr (Array xs) = do
+                        vs <- mapM evalExpr xs
+                        return (ArrayVal vs)
+evalExpr Undefined = return UndefinedVal
 evalExpr TrueConst = return TrueVal
 evalExpr FalseConst = return FalseVal
 evalExpr (Var ident) = getVar ident
-evalExpr (Compr ar) = undefined --TODO
-evalExpr (Call fname params) = do (ArrayVal values) <- evalExpr (Array params)
+evalExpr (Compr ar) = evalArrayComp ar
+evalExpr (Call fname params) = do
+                                  (ArrayVal values) <- evalExpr (Array params)
                                   function <- getFunction fname
                                   case function values of
                                     (Left e) -> fail e
                                     (Right val) -> return val
-evalExpr (Assign ident expr) = do value <- evalExpr expr
-                                  putVar ident value
-                                  return value
-evalExpr (Comma exp1 exp2) = do evalExpr exp1
-                                evalExpr exp2
+evalExpr (Assign ident expr) = do
+                                value <- evalExpr expr
+                                putVar ident value
+                                return value
+evalExpr (Comma exp1 exp2) = do
+                               _ <- evalExpr exp1
+                               evalExpr exp2
 
+-- Helper function to deal with List Comprehensions
+evalArrayComp :: ArrayCompr -> SubsM Value
+evalArrayComp (ACFor ident expr ac) = do
+                                        iteratorValue <- evalExpr expr
+                                        -- backup old variable
+                                        maybeOldVar <- getMaybeVar ident
+                                        eval <- evalArrayCompFor
+                                                  ident iteratorValue ac
+                                        -- restore old variable or cleanup
+                                        case maybeOldVar of
+                                          (Just oldVar) -> putVar ident oldVar
+                                          Nothing -> deleteVar ident
+                                        return eval
+evalArrayComp (ACBody expr) =  evalExpr expr
+evalArrayComp (ACIf expr ac) = do
+                                  condition <- evalExpr expr
+                                  case condition of
+                                    TrueVal -> evalArrayComp ac
+                                    FalseVal -> return (ArrayVal [])
+                                    _ -> fail ("condition in if clause is " ++
+                                               "not a boolean")
+
+-- Helper function to deal with List Comprehensions.
+-- This function does the iteration
+evalArrayCompFor :: Ident -> Value -> ArrayCompr -> SubsM Value
+evalArrayCompFor _ (ArrayVal []) _ = return (ArrayVal [])
+evalArrayCompFor ident (ArrayVal (x:xs)) ac =
+    do
+      putVar ident x
+      mv <- evalArrayComp ac
+      (ArrayVal mvs) <- evalArrayCompFor ident (ArrayVal xs) ac
+      -- accounting for nested for:
+      case mv of
+        (ArrayVal mv') -> return (ArrayVal (mv' ++ mvs))
+        _ -> return (ArrayVal (mv:mvs))
+evalArrayCompFor ident (StringVal s) ac =
+    evalArrayCompFor ident (ArrayVal [StringVal [x] | x <- s]) ac
+evalArrayCompFor _ _ _ =
+    fail "Array Comprehension iterator has to be an Array or String"
 
 
 runExpr :: Expr -> Either Error Value
 runExpr expr = case runSubsM (evalExpr expr) initialContext of
                  Left e -> Left $ "Interpreter failed: " ++ e
                  Right (val, _) -> Right val
-  
-  
