9a8efc38216ae440063b02185c701ef3c0d26cf1
KonradUCPH <mbg670@alumni.ku.dk>
2017-09-28 14:59:12 +0200

    add test cases for like and dislike

--- assignment3/src/test.pl
+++ assignment3/src/test.pl
@@ -1,9 +1,26 @@
+:- begin_tests(twitbook).
+
 g1([person(kara, [barry, clark]),
  person(bruce, [clark, oliver]),
  person(barry, [kara, oliver]),
  person(clark, [oliver, kara]),
  person(oliver, [kara])]).
 
-testLikes() :-
-    g1(G),
-    likes(bruce)
+test(like, all(_ = [1])) :-
+    g1(G), likes(G,bruce,clark).
+
+test('doesnt like', all(_ = [1])) :-
+    g1(G), \+likes(G,bruce,kara).
+
+test(disLike, all(_ = [1])) :-
+    g1(G), dislikes(G,clark,bruce).
+
+test('doesnt disLike', all(_ = [1])) :-
+    g1(G), \+dislikes(G,bruce,clark).
+
+:- end_tests(twitbook).
+
+% Imports by regular file import
+% Run tests by run_tests
+
+% The way to suppress Choicpoint warning all(_ = [1])
