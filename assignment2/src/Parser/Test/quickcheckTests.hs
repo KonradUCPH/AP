752c3b10579ff8aa88f40211f69ef77dabf01c22
SteffenCzolbe <steffen.czolb@gmail.com>
2017-10-04 21:43:13 +0200

    added a working quickcheck example for generating array expressions

--- assignment2/src/Parser/Test/quickcheckTests.hs
+++ assignment2/src/Parser/Test/quickcheckTests.hs
@@ -3,35 +3,39 @@ module Main (main, spec) where
 import Test.QuickCheck
 import Test.Hspec
 import qualified Data.Char as C
+import Control.Monad
 
 import SubsAst
 import SubsParser
 
 --import Control.Monad (liftM)
 
-genJsString :: Gen String
-genJsString = listOf $ elements $ [' '..'['] ++ [']'..'~'] -- printable acci characters minus '\'
+
 
 
 instance Arbitrary Expr where
-    arbitrary = expr
+    arbitrary = sized exprN
 
-expr = sized exprN
 exprN 0 = fmap Number arbitrary
-exprN _ = oneof [ fmap Number arbitrary,
+exprN n = oneof [ fmap Number arbitrary,
                   fmap String genJsString,
-                  fmap Array arbitrary
-                --, liftM Call "+" [subexpr, subexpr] -- we ran out of time here :(
-                --, liftM2 Minus subexpr subexpr
+                  arbitraryArray n
                 ]
             --where subexpr = exprN (n `div` 2)
--- checks for valid string characters
-validJsString :: String -> Bool
-validJsString = all $ \c -> c `elem` [' '..'~'] && (c /= '\\')
 
-dummyTest :: Expr -> Bool
-dummyTest _ = True
+-- generates an Arbitrary Array expression, limited by length n
+arbitraryArray :: Int -> Gen Expr
+arbitraryArray n = do
+                    (Positive m) <- arbitrary -- get positive number
+                    let n' = n `div` (m + 1)
+                    a <- replicateM m (exprN n')
+                    return $ Array a
 
+-- generate arbitrary valid JavaScript string
+arbitraryJsString :: Gen String
+arbitraryJsString = listOf $ elements $ [' '..'['] ++ [']'..'~'] -- printable asci characters minus '\'
+
+-- transform an expression into JavaScript code
 exprToJs :: Expr -> String
 exprToJs (String s) = "\'" ++ s ++ "\'"
 exprToJs (Number n) = show n
@@ -43,6 +47,8 @@ arrayToJs a True = "[" ++ arrayToJs a False
 arrayToJs [] False = "]"
 arrayToJs [a] False = exprToJs a ++ "]"
 arrayToJs (h:t) False = exprToJs h ++ ", " ++ arrayToJs t False
+
+
 
 testRandomizedExpression :: Expr -> Bool
 testRandomizedExpression expr = case parseString (exprToJs expr) of
