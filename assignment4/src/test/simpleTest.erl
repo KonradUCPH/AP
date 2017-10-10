146d8d5fbe26a8d0349a2e270e746966e76fba45
SteffenCzolbe <steffen.czolb@gmail.com>
2017-10-10 18:39:29 +0200

    clean up of test files

--- assignment4/src/test/simpleTest.erl
+++
@@ -1,8 +0,0 @@
--module(simpleTest).
--export([test/0]).
-
-test() ->
-    {ok, S} = kaboose:start(),
-    {ok, R} = kaboose:get_a_room(S),
-    kaboose:add_question(R, {"Who made this file?", ["noone", {correct, "someone"}, "someone else"]}),
-    {_ActvRm, _Cref} = kaboose:play(R).
